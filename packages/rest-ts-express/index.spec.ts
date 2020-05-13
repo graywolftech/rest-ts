731d27bce6e3ec3c850660cb8540c973c660763d
Jacob Smith <jacob.smith@unb.ca>
2020-05-13 10:45:44 -0300

    added cjs build for packages

--- packages/rest-ts-express/index.spec.ts
+++ packages/rest-ts-express/index.spec.ts
@@ -1,5 +1,8 @@
+import express from "express";
+import { TypedAsyncRouter } from "./index";
+
 // Not a real test but it does test the types
-// const app = express();
+const app = express();
 // const router = TypedAsyncRouter<{ "/test": { POST: { body: { j: 3 }; response: string } } }>(app);
 
 // router.post("/test", async (req) => {
@@ -18,18 +21,18 @@
 //   return "" + req.body.j * 2;
 // });
 
-// export type API = {
-//   "/plant-potato": {
-//     POST: {
-//       body: { size: number; weight: number };
-//       response: { result: "success" | "error" };
-//     };
-//   };
-// };
+export type API = {
+  "/plant-potato": {
+    POST: {
+      body: { size: number; weight: number };
+      response: { result: "success" | "error" };
+    };
+  };
+};
 
-// const router = TypedAsyncRouter<API>(app);
-// router.post("/plant-potato", async (req) => {
-//   return {
-//     result: "potato",
-//   };
-// });
+const router = TypedAsyncRouter<API>(app);
+router.post("/plant-potato", async () => {
+  return {
+    result: "success",
+  };
+});
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,22 @@
 import typescript from "@rollup/plugin-typescript";
 
-export const config = {
-  input: "index.ts",
-  output: {
-    dir: "dist",
-    format: "module",
+export const config = [
+  {
+    input: "index.ts",
+    output: {
+      // dir: "dist",
+      file: "dist/index.es.js",
+      format: "es",
+    },
+    plugins: [typescript()],
   },
-  plugins: [typescript()],
-};
+  {
+    input: "index.ts",
+    output: {
+      // dir: "dist",
+      file: "dist/index.cjs.js",
+      format: "cjs",
+    },
+    plugins: [typescript()],
+  },
+];
